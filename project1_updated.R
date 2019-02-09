21c7445d5b35d0fc1116809e0404494ac26d7633
Tony <chao_huang@live.com>
2019-02-09 12:20:15 -0800

    project1_updated.R with many bugs fixed

--- project1_updated.R
+++ project1_updated.R
@@ -30,84 +30,71 @@ print(paste("Whole Life Net Single Premium: ", nsp))
 
 
 
+#-------------------Calculating Probability of Dead Curve--------------
+CalculateFinalAge <- function(inputAge) { 
+  
 
-
-
-
-
-#----------now creating a block of 10000 people who are in diffrent ages and want diffrent benefits
-
-
-BussinessBlock <- function(rAge,rBenefit) { 
-  netSinglePremium <- lifeTable[lifeTable$ages == rAge, c("A_x")]*rBenefit
-  return(netSinglePremium)
-}
-
-bAge <- 0
-bBen <- 0
-bNps <- 0
-bFAge <- 0
-maxAges <- max(lifeTable$ages)
-lifeTableAges <- lifeTable$ages[ages < maxAges]# avoid picking max ages
-
-inputNumberClients <- as.numeric(inputsProject1[inputsProject1$label == "inputNumberClients", c("value")])
-
-# ---------------------- looping 10000 -------------------------------------
-#lifeTableAges <- data.frame(Age=ages) # creating a data frame that contains the ages 
-
-print("---------Calculating Lifetimes-----------")
-print(paste("Number of clients: ", inputNumberClients))
-# for (i in 1:inputNumberClients){ # 10,000 (whole life) incurances with Net Single Premium
-#   
-#   randomAge <- 2
-#   #randomAge <-  sample(lifeTableAges, 1) 
-#   randomBenefit <- 1000 #testing with only 1000 us for all the clients
-#   #randomBenefit <- sample.int(9000, 1, replace=TRUE) + 1000 # picking one randonm integer from range $1000-$1000000 benefit
-#   bAge[i] <- randomAge # concatenate
-#   bBen[i] <- randomBenefit # concatenate
-#   bNps[i] <- BussinessBlock(randomAge,randomBenefit) # calling the function to calculate the net single premium then concatenate
-# 
-#   #--------calculate random dies based on mortality table ----------   this part can be improved in efficiency
-#   #calculate probability of (x) lives t years  t_p_x where x = inputAges
-#   previousAgeP <- lifeTable[ages == (randomAge - 1), c("t_p_x0")]
-#   if (randomAge > 0) {
-#     pLives <- lifeTable[ages >= randomAge, c("t_p_x0")]/previousAgeP
-#   } else {
-#     pLives <- lifeTable[ages >= randomAge, c("t_p_x0")]
-#   }
-#   
-#   pLivesAges <- lifeTable[ages >= randomAge, c("ages")]
-#   pCurveX <- data.frame(pLivesAges, pLives)
-#   
-#   #calculate probability tl1_q_x that x survives t years and dies within 1 year
-#   pCurveXRow <- nrow(pCurveX)
-#   
-#   #probability of dead based on input age
-#   for (j in 1:(pCurveXRow-1)) {
-#     pCurveX$tl1_q_x[j] <- pCurveX$pLives[j] - pCurveX$pLives[j+1] #i = u in the pdf   1 = t in pdf
-#   }
-#   pCurveX$tl1_q_x[pCurveXRow] <- pCurveX$pLives[pCurveXRow] # last line
-#   
-#   
-#   #generate random dies based on input age
-#   randomFinalAge <- sample(pCurveX$pLivesAges, 1, replace = T, pCurveX$tl1_q_x)
-#   bFAge[i] <- randomFinalAge
-#   
-#   #-----this simulation part is for testing, do not delete-----------
-#   #myGraph <- ggplot(pCurveX, aes(pLivesAges, tl1_q_x))
-#   #print(myGraph + geom_point() + geom_point(aes(x=randomFinalAge, y=0.02), color="red"))
-#   #Sys.sleep(2)
-# }
-randomAge <- 0
-bAge <- rep(randomAge, inputNumberClients) # concatenate
-bBen <- rep(100, inputNumberClients) # concatenate
-n <- length(bAge)
-for (k in 1:n){
-  bNps[k] <- BussinessBlock(bAge[k], bBen[k])
+  #-------------------bug fixed---------------
+  
+  #calculate probability of (x) lives t years  t_p_x where x = inputAges
+  previousAgeP <- lifeTable[ages == (inputAge - 1), c("t_p_x0")]
+  if (inputAge > 0) {
+    pLives <- lifeTable[ages >= inputAge, c("t_p_x0")]/previousAgeP
+  } else {
+    pLives <- lifeTable[ages >= inputAge, c("t_p_x0")]
+  }
+  
+  pLivesAges <- lifeTable[ages >= inputAge, c("ages")]
+  pCurveX <- data.frame(pLivesAges, pLives)
+  
+  #calculate probability tl1_q_x that x survives t years and dies within 1 year
+  pCurveXRow <- nrow(pCurveX)
+  
+  #probability of dead based on input age
+  pCurveX$tl1_q_x[1] <- 1 - pCurveX$pLives[1] # firt data important,,, to avoid bug
+  if(pCurveXRow > 1) { #bug -fixed,  avoid enter for when nrow(pCurveX) == 0, this occur when inputAge is the last value of the tablelife 
+    for (j in 2:(pCurveXRow)) {
+      
+      #small bug which does not consider the 0|1_q_x data..... also add q_x of 1 in the last year automatically? 
+      pCurveX$tl1_q_x[j] <- pCurveX$pLives[j-1] - pCurveX$pLives[j]
+      #pCurveX$tl1_q_x[j] <- pCurveX$pLives[j] - pCurveX$pLives[j+1] #i = u in the pdf   1 = t in pdf
+    }
+  }
+  #----------------------------------
+  
+  
+  #print(sum(pCurveX$tl1_q_x))
+  
+  #plot lives probability based on input age
+  #myGraph <- ggplot(pCurveX, aes(pLivesAges, pLives))
+  #myGraph + geom_point()
+  #ggsave(filename = "images/lives probability based on input age.png", plot = myGraph)
+  
+  #x11()
+  #plot tl1_q_x based on input age
+  #myGraph <- ggplot(pCurveX, aes(pLivesAges, tl1_q_x))
+  #myGraph <- myGraph + geom_point() + labs(title = "probability of (x) survive t years and die next year")
+  #print(myGraph)
+  #ggsave(filename = "images/probability of (X) survive t years and die next year.png", plot = myGraph)
+  #----------------------------------------------
+  
+  #generate random dies based on input age
+  finalAge <- 0
+  if(pCurveXRow > 1) { #bug -fixed,  avoid enter when nrow(pCurveX) == 0, this occur when inputAge is the last value of the tablelife
+    finalAge <- sample(pCurveX$pLivesAges, 1, replace = T, pCurveX$tl1_q_x)
+  }else
+  {
+    finalAge <- pCurveX$pLivesAges[1] # only has 1 probability
+  }
+  
+  #ttt <- data.frame(s)
+  #myGraph <- ggplot(ttt, aes(s))
+  #myGraph + geom_histogram()
+  return (finalAge)
 }
 
 
-CalculateFinalAgePerfectData <- function(inputAge) { 
+CalculateFinalAgePerfectData <- function(inputAge) { #---only for testing
   
   #calculate probability of (x) lives t years  t_p_x where x = inputAges
   previousAgeP <- lifeTable[ages == (inputAge - 1), c("t_p_x0")]
@@ -142,7 +129,7 @@ CalculateFinalAgePerfectData <- function(inputAge) {
   fIndex <- 0 #final index
   counter <- 0
   for(k in 1:pCurveXRow){
-
+    
     nRepeat <- pCurveX$tl1_q_x[k]*inputNumberClients
     if(nRepeat%%1 >= 0.5){ # use this to avoid number of data > pCurveXRow
       if(counter%%2 == 0){
@@ -166,12 +153,12 @@ CalculateFinalAgePerfectData <- function(inputAge) {
   perfectData[(fIndex+1):inputNumberClients] <- sample(pCurveX$pLivesAges, nFill, replace = T, pCurveX$tl1_q_x)
   #perfectData[(nData+1):inputNumberClients] <- perfectData[nData]
   
+  #return a vector
   return(perfectData)
 }
 
 
-
-CalculateFinalAgeSemiPerfectData <- function(inputAge) { 
+CalculateFinalAgeSemiPerfectData <- function(inputAge) { #---only for testing
   
   #calculate probability of (x) lives t years  t_p_x where x = inputAges
   previousAgeP <- lifeTable[ages == (inputAge - 1), c("t_p_x0")]
@@ -197,6 +184,8 @@ CalculateFinalAgeSemiPerfectData <- function(inputAge) {
   }
   #pCurveX$tl1_q_x[pCurveXRow] <- pCurveX$pLives[pCurveXRow] # last line
   
+  
+  #note: return a vector
   bFAge <- sample(pCurveX$pLivesAges, inputNumberClients, replace = T, pCurveX$tl1_q_x)
   
   
@@ -206,7 +195,61 @@ CalculateFinalAgeSemiPerfectData <- function(inputAge) {
 
 
 
-bFAge <- CalculateFinalAgePerfectData(randomAge)
+
+
+#----------now creating a block of 10000 people who are in diffrent ages and want diffrent benefits
+
+
+BussinessBlock <- function(rAge,rBenefit) { 
+  netSinglePremium <- lifeTable[lifeTable$ages == rAge, c("A_x")]*rBenefit
+  return(netSinglePremium)
+}
+
+bAge <- 0
+bBen <- 0
+bNps <- 0
+bFAge <- 0
+maxAges <- max(lifeTable$ages)
+lifeTableAges <- lifeTable$ages[ages < maxAges]# avoid picking max ages
+
+inputNumberClients <- as.numeric(inputsProject1[inputsProject1$label == "inputNumberClients", c("value")])
+
+#---------------------- looping 10000 -------------------------------------
+lifeTableAges <- data.frame(Age=ages) # creating a data frame that contains the ages
+
+print("---------Calculating Lifetimes-----------")
+print(paste("Number of clients: ", inputNumberClients))
+for (i in 1:inputNumberClients){ # 10,000 (whole life) incurances with Net Single Premium
+
+
+  randomAge <-  sample(lifeTableAges$Age, 1, replace = T) # there was a bug before lifeTableAges instead of lifeTableAges$Age
+  randomBenefit <- sample.int(9000, 1, replace=TRUE) + 1000 # picking one randonm integer from range $1000-$1000000 benefit
+  bAge[i] <- randomAge # concatenate
+  bBen[i] <- randomBenefit # concatenate
+  bNps[i] <- BussinessBlock(randomAge,randomBenefit) # calling the function to calculate the net single premium then concatenate
+
+  #--------calculate random dies based on mortality table ----------   this part can be improved in efficiency
+
+  bFAge[i] <- CalculateFinalAge(randomAge)
+
+  #-----this simulation part is for testing, do not delete-----------
+  #myGraph <- ggplot(pCurveX, aes(pLivesAges, tl1_q_x))
+  #print(myGraph + geom_point() + geom_point(aes(x=randomFinalAge, y=0.02), color="red"))
+  #Sys.sleep(2)
+}
+
+
+
+
+# #---only for testing
+# randomAge <- 0
+# bAge <- rep(randomAge, inputNumberClients) # concatenate
+# bBen <- rep(100, inputNumberClients) # concatenate
+# for (k in 1:inputNumberClients){
+#   bNps[k] <- BussinessBlock(bAge[k], bBen[k])
+# }
+# bFAge <- CalculateFinalAgePerfectData(randomAge)
+
 
 bDataFrame <- data.frame(Age = bAge, Benefit = bBen, NetSinglePremium = bNps, Die = bFAge) #Creating the final dataframe
 bDataFrame$SurviveYears <- bDataFrame$Die - bDataFrame$Age
@@ -237,7 +280,7 @@ nYears <- max(paymentTable$SurviveYears)
 
 for (i in 1:nYears) {
   surviveYears[i+1] <- i
-
+  
   money[i+1] <- money[i] + earnInterest[i] - benefitPayment[i]
   
   if(money[i+1] > 0){ #only calculate earnInterest when money > 0
